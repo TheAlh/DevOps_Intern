80c506dee4c3118d8473a3a485bfd8835bcdaac1
Egor Vasinev <106512748+TheAlh@users.noreply.github.com>
2022-06-07 09:44:22 +0700

    Update TaskN1.py

--- TaskN1.py
+++ TaskN1.py
@@ -15,9 +15,9 @@ with open('log.txt', 'r') as inf: #We output a line and if it contains the desir
 output = input('Print to screen (y), otherwise save to log-out.txt: (y/n) ') #We display the received sid on the screen, or in the file log-out.txt
 if output == 'y':
     for i in range(len(sid)):
-        print(f'{ip} - sid={sid[i]}') #Here you can change the output method and output, for example, only sid. ( ouf.write(f'{sid[i]}\n'))
+        print(f'{sid[i]}\n') #Here you can change the output method and output, for example, like this: ( print(f'{ip} - sid={sid[i]}\n') )
     input('Press ENTER to exit')
 else:
     with open('log-out.txt', 'w') as ouf:
         for i in range(len(sid)):
-            ouf.write(f'{ip} - sid={sid[i]}\n') #Here you can change the output method and output, for example, only sid. ( ouf.write(f'{sid[i]}\n') )
+            ouf.write(f'{sid[i]}\n') #Here you can change the output method and output, for example, like this: ( ouf.write(f'{ip} - sid={sid[i]}\n') )
